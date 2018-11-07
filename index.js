ea3ec686dedc66eac650fa62a11bd896bbee0e79
Gonzalo Burgos <pelaogon@gmail.com>
2018-11-07 12:25:56 -0300

    capitulo02 usando mongodb con node y graphql

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
+import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
 
-import typeDefs from './schemas';
-import resolvers from './resolvers';
+import models from './models';
+
+//mezclar todos los archivos ded carpetas de types y resolvers
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers} from 'merge-graphql-schemas';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './types')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
+
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -15,9 +24,23 @@ const PORT = 3000;
 const app = express();
 
 //bodyParser is needed just for POST.
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema,
+  context: {
+    models,
+    user: {
+      _id: 1, username: 'gonzalo'
+    }
+  }
+}));
 app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want graphiql enabled
 
-app.listen(PORT, () => {
-  console.log('Running server...');
-});
+//Conexion BD
+mongoose.connect('mongodb://localhost:27017/instagram-clone', { useNewUrlParser: true }).then(
+  () => {
+    console.log('Conectado a Mongo!!');
+    app.listen(PORT, () => {
+      console.log('Running server...');
+    });
+  }
+)
